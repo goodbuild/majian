074ac99f235239ffc4991e138421d57ba9e30282
wanghuacong <wanghuacong@langlib.com>
2018-09-30 15:49:29 +0800

    最新胡牌

--- src/config/Config.java
+++ src/config/Config.java
@@ -9,6 +9,7 @@ public class Config {
     public static final int MAX_GAMG_NUM_8 = 8;
     public static final int MAX_GAMG_NUM_16 = 8;
 
+    // 所有牌的比较都是按照正常的逻辑，从左到右比较对
     public static final int RANGE_PENG = 0;
     public static final int RANGE_GANG = 0;
     public static final int RANGE_BC = -1;
